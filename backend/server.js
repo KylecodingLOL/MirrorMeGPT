fab8d49fedbe8c1ed4a3f3a00e05d39bc5888372
kylecoding1 <128002901+kylecoding1@users.noreply.github.com>
2023-09-07 13:59:22 -0700

    .

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3002;
 
 app.set('trust proxy', 1);
 const apiLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // 10 minutes
+    windowMs: 10 * 60 * 1000, 
     max: 10, 
     handler: function (req, res) {
         const rateResetTime = req.rateLimit.resetTime ? new Date(req.rateLimit.resetTime).toISOString() : null;
@@ -29,7 +29,7 @@ const apiLimiter = rateLimit({
     }
 });
 
-// Apply the rate-limiting middleware to all routes
+
 app.use("/generate", apiLimiter);
 
 
@@ -37,7 +37,7 @@ app.use("/generate", apiLimiter);
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://www.mirrormegpt.com',
+    origin: 'http://localhost:3002',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -55,7 +55,7 @@ app.get('/favicon.ico', (req, res) => {
 
 app.post('/generate', async (req, res) => {
     const { prompt, rules } = req.body;
-    const apiKey = process.env.OPENAI_API_KEY;  // Loaded from .env file
+    const apiKey = process.env.OPENAI_API_KEY;  
     const model = "gpt-3.5-turbo";
 
     const conversation = [
@@ -72,37 +72,37 @@ app.post('/generate', async (req, res) => {
         "Authorization": `Bearer ${apiKey}`
     },
     body: JSON.stringify({
-        model: "gpt-3.5-turbo-0613",  // Add this line to specify the model
-        messages: conversation,  // Use the 'messages' parameter for chat-based API
+        model: "gpt-3.5-turbo-0613",  
+        messages: conversation,  
         max_tokens: 2000
     })
-}).catch(err => console.error("Fetch Error: ", err));  // Debug log for fetch errors
+}).catch(err => console.error("Fetch Error: ", err));  
 
 
     const data = await response.json();
-    console.log("Response from OpenAI: ", data);  // Debug log for OpenAI response
+    console.log("Response from OpenAI: ", data); 
 
     let content = "Error generating text";
     if (data.choices && data.choices.length > 0) {
         content = data.choices[0]?.message?.content?.trim() || "Error generating text";
     }
 
-    // Format the generated text to alternate between Four-Per-Em Space and regular space
+
     let newContent = '';
-    let useSpecialSpace = true;  // Flag to alternate between special and regular space
-    let useSpecialA = true;  // Flag to alternate between regular 'a' and Cyrillic 'а'
-    let useSpecialO = true;  // Flag to alternate between regular 'o' and Greek 'ο'
+    let useSpecialSpace = true;  
+    let useSpecialA = true;  
+    let useSpecialO = true; 
 
     for (let i = 0; i < content.length; i++) {
         if (content[i] === ' ') {
             newContent += useSpecialSpace ? '\u2004' : ' ';
-            useSpecialSpace = !useSpecialSpace;  // Toggle the flag
+            useSpecialSpace = !useSpecialSpace;  
         } else if (content[i].toLowerCase() === 'a') {  
             newContent += useSpecialA ? 'а' : 'a';  
-            useSpecialA = !useSpecialA;  // Toggle the flag
+            useSpecialA = !useSpecialA;  
         } else if (content[i].toLowerCase() === 'o') {  
             newContent += useSpecialO ? 'ο' : 'o';  
-            useSpecialO = !useSpecialO;  // Toggle the flag
+            useSpecialO = !useSpecialO;  
         } else {
             newContent += content[i];
         }
